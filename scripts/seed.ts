a628fb670c7be6c9a090751594ea678afbf25da2
bhki29 <ajiahmadbaehaki20@gmail.com>
2025-05-01 23:06:43 +0700

    fetch units from database

--- scripts/seed.ts
+++ scripts/seed.ts
@@ -13,6 +13,12 @@ const main = async () => {
 
     await db.delete(schema.courses);
     await db.delete(schema.userProgress);
+    await db.delete(schema.units);
+    await db.delete(schema.lessons);
+    await db.delete(schema.challenges);
+    await db.delete(schema.challengeOptions);
+    await db.delete(schema.challengeProgress);
+
 
     await db.insert(schema.courses).values([
       {
@@ -39,6 +45,62 @@ const main = async () => {
         id: 5,
         title: "PHP",
         imageSrc: "/php.png",
+      },
+    ]);
+
+    await db.insert(schema.units).values([
+      {
+        id: 1,
+        courseId: 1, //HTML
+        title: "Unit 1",
+        description: "Belajar dasar HTML",
+        order: 1,
+      }
+    ]);
+
+    await db.insert(schema.lessons).values([
+      {
+        id: 1,
+        unitId: 1, // Unit 1 (Belajar dasar Html...)
+        order: 1,
+        title: "Nouns"
+      },
+    ]);
+
+    await db.insert(schema.challenges).values([
+      {
+        id: 1,
+        lessonId: 1, // Nouns
+        type: "SELECT",
+        order: 1,
+        question: 'wich one of these is the "the man"?'
+      },
+    ]);
+
+    await db.insert(schema.challengeOptions).values([
+      {
+        id: 1,
+        challengeId: 1, // wich one of these is the "the man"?
+        imageSrc: "/html.png",
+        correct: true,
+        text: "el hombre",
+        audioSrc: "/es_man.mp3"
+      },
+      {
+        id: 2,
+        challengeId: 1, // wich one of these is the "the man"?
+        imageSrc: "/html.png",
+        correct: false,
+        text: "la mujer",
+        audioSrc: "/es_woman.mp3"
+      },
+      {
+        id: 3,
+        challengeId: 1, // wich one of these is the "the man"?
+        imageSrc: "/html.png",
+        correct: false,
+        text: "el robot",
+        audioSrc: "/es_woman.mp3"
       },
     ]);
 
