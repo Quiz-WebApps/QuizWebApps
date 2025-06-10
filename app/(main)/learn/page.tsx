912153c6052d56fca24fac463f471e30013cdfc4
bhki29 <ajiahmadbaehaki20@gmail.com>
2025-06-10 23:07:07 +0700

    create course progress

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,26 +1,45 @@
 import { redirect } from "next/navigation";
 
-import { getUnits, getUserProgress } from "@/db/queries";
+import { lessons, units as unitsSchema } from "@/db/schema";
 import { FeedWrapper } from "@/components/feed-wrapper";
 import { UserProgress } from "@/components/user-progress";
 import { StickyWrapper } from "@/components/sticky-wrapper";
+import { 
+  getCourseProgress, 
+  getLessonPercentage, 
+  getUnits, 
+  getUserProgress 
+} from "@/db/queries";
 
 import { Header } from "./header";
 import { Unit } from "./unit";
 
+
 const LearnPage = async () => {
   const userProgressData = getUserProgress();
+  const courseProgressData = getCourseProgress();
+  const lessonPercentageData = getLessonPercentage();
   const unitsData = getUnits();
 
   const [
     userProgress,
     units,
+    courseProgress,
+    lessonPercentage
   ] = await Promise.all([
     userProgressData,
     unitsData,
+    courseProgressData,
+    lessonPercentageData,
   ]);
 
-  if (!userProgress || !userProgress.activeCourse) redirect("/courses");
+  if (!userProgress || !userProgress.activeCourse) {
+    redirect("/courses");
+  };
+
+  if (!courseProgress) {
+    redirect("/courses");
+  }
 
   return (
     <div className="flex flex-row-reverse gap-[48px] px-6">
@@ -42,8 +61,11 @@ const LearnPage = async () => {
               description={unit.description}
               title={unit.title}
               lessons={unit.lessons}
-              activeLesson={undefined}
-              activeLessonPercentage={0}
+              activeLesson={courseProgress.activeLesson as typeof lessons.
+                $inferSelect & {
+                  unit: typeof unitsSchema.$inferSelect;
+                } | undefined }
+              activeLessonPercentage={lessonPercentage}
             />
           </div>
         ))}
