38b0b325f4b122aebde68827786964d580cab949
bhki29 <ajiahmadbaehaki20@gmail.com>
2025-06-15 17:18:30 +0700

    creaate feature lesson header and adding componen shadcn@latest progress

--- db/queries.ts
+++ db/queries.ts
@@ -57,14 +57,21 @@ export const getUnits = cache(async () => {
   });
 
   const normalizedData = data.map((unit) => {
-    const lessonsWithCompletedStatus = unit.lessons.map((lessons) => {
-      const allCompletedChallenges = lessons.challenges.every((challenge)=> {
+    const lessonsWithCompletedStatus = unit.lessons.map((lesson) => {
+      if (
+        lesson.challenges.length === 0
+      ) {
+        return { ...lesson, completed: false};
+      }
+
+      const allCompletedChallenges = lesson.challenges.every((challenge)=> 
+        {
         return challenge.challengeProgress
           && challenge.challengeProgress.length > 0
           && challenge.challengeProgress.every((progress) => progress.completed);
       });
 
-      return {...lessons, completed: allCompletedChallenges };
+      return {...lesson, completed: allCompletedChallenges };
     });
 
     return { ...unit, lessons: lessonsWithCompletedStatus};
