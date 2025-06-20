2e4f36a5b561936fa32d02fda6a6edb0eaafd2a2
wandajulianto <wandajuianto37@gmail.com>
2025-06-21 02:41:01 +0700

    feat: enhance quiz system and remove audio features

--- app/lesson/quiz.tsx
+++ app/lesson/quiz.tsx
@@ -1,15 +1,23 @@
 "use client";
 
-import { useState } from "react";
+import { toast } from "sonner";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { useState, useTransition } from "react";
+import Confetti from "react-confetti";
+import { useWindowSize, useMount } from "react-use";
 
-import { challengeOptions, challenges } from "@/db/schema";
+import { reduceHearts } from "@/actions/user-progress";
+import { useHeartsModal } from "@/store/use-hearts-modal";
+import { usePracticeModal } from "@/store/use-practice-modal";
+import { challengeOptions, challenges, userSubscription } from "@/db/schema";
+import { upsertChallengeProgress } from "@/actions/challenge-progress";
 
 import { Header } from "./header";
 import { QuestionBubble } from "./question-bubble";
 import { Challenge } from "./challenge";
 import { Footer } from "./footer";
-
-
+import { ResultCard } from "./result-card";
 
 type Props = {
   initialPercentage: number;
@@ -19,30 +27,53 @@ type Props = {
     completed: boolean;
     challengeOptions: typeof challengeOptions.$inferSelect[];
   })[];
-  userSubscription: any;
+  userSubscription: typeof userSubscription.$inferSelect & {
+    isActive: boolean;
+  } | null;
 };
 
-
 export const Quiz = ({
-    initialPercentage,
-    initialHearts,
-    initialLessonId,
-    initialLessonChallenges,
-    userSubscription
+  initialPercentage,
+  initialHearts,
+  initialLessonId,
+  initialLessonChallenges,
+  userSubscription,
 }: Props) => {
-const [hearts, setHearts] = useState(initialHearts);
-const [percentage, setPercentge] = useState(initialPercentage);
-const [challenges] = useState(initialLessonChallenges);
-const [activeIndex, setActiveIndex] = useState(() => {
+  const { open: openHeartsModal } = useHeartsModal();
+  const { open: openPracticeModal } = usePracticeModal();
+
+  useMount(() => {
+    if (initialPercentage === 100) {
+      openPracticeModal();
+    }
+  });
+  
+  const { width, height } = useWindowSize();
+
+  const router = useRouter();
+
+  const [pending, startTransition] = useTransition();
+
+  const [lessonId] = useState(initialLessonId);
+  const [hearts, setHearts] = useState(initialHearts);
+  const [percentage, setPercentage] = useState(() => {
+    return initialPercentage === 100 ? 0 : initialPercentage;
+  });
+  const [challenges] = useState(initialLessonChallenges);
+  const [activeIndex, setActiveIndex] = useState(() => {
     const uncompletedIndex = challenges.findIndex((challenge) => !challenge.completed);
     return uncompletedIndex === -1 ? 0 : uncompletedIndex;
   });
 
-  const [selectedOption, setSelectedOption ] = useState<number>();
+  const [selectedOption, setSelectedOption] = useState<number>();
   const [status, setStatus] = useState<"correct" | "wrong" | "none">("none");
 
   const challenge = challenges[activeIndex];
   const options = challenge?.challengeOptions ?? [];
+
+  const onNext = () => {
+    setActiveIndex((current) => current + 1);
+  };
 
   const onSelect = (id: number) => {
     if (status !== "none") return;
@@ -50,22 +81,118 @@ const [activeIndex, setActiveIndex] = useState(() => {
     setSelectedOption(id);
   };
 
-  const title = challenge.type === "ASSIST"
-    ? "Select the correct meaning"
-    : challenge.question;
+  const onContinue = () => {
+    if (!selectedOption) return;
 
-    return (
+    if (status === "wrong") {
+      setStatus("none");
+      setSelectedOption(undefined);
+      return;
+    }
+
+    if (status === "correct") {
+      onNext();
+      setStatus("none");
+      setSelectedOption(undefined);
+      return;
+    }
+
+    const correctOption = options.find((option) => option.correct);
+
+    if (!correctOption) return;
+
+    if (correctOption.id === selectedOption) {
+      startTransition(() => {
+        upsertChallengeProgress(challenge.id).then((response) => {
+          if (response?.error === "hearts") {
+            openHeartsModal();
+            return;
+          }
+
+          setStatus("correct");
+          setPercentage((prev) => prev + 100 / challenges.length);
+
+          if (initialPercentage === 100) {
+            setHearts((prev) => Math.min(prev + 1, 5));
+          }
+        }).catch(() => toast.error("Something went wrong. Please try again."))
+      });
+    } else {
+      startTransition(() => {
+        reduceHearts(challenge.id).then((response) => {
+          if (response?.error === "hearts") {
+            openHeartsModal();
+            return;
+          }
+
+          setStatus("wrong");
+
+          if (!response?.error) {
+            setHearts((prev) => Math.max(prev - 1, 0));
+          }
+        }).catch(() => toast.error("Somethin went wrong. Please try again."));
+      });
+    }
+  };
+
+  return (
+    <>
+      {!challenge ? (
         <>
-        <Header
-        hearts={hearts}
-        percentage={percentage}
-        hasActiveSubscription={!!userSubscription?.isActive}
-        />
-        <div className="flex-1">
+          <Confetti
+            width={width}
+            height={height}
+            recycle={false}
+            numberOfPieces={500}
+            tweenDuration={10000}
+          />
+          <div className="flex flex-col gap-y-4 lg:gap-y-8 max-w-lg mx-auto text-center items-center justify-center h-full">
+            <Image
+              src="/finish.png"
+              alt="Finish"
+              className="hidden lg:block"
+              height={100}
+              width={100}
+            />
+            <Image
+              src="/finish.png"
+              alt="Finish"
+              className="block lg:hidden"
+              height={50}
+              width={50}
+            />
+            <h1 className="text-xl lg:text-3xl font-bold text-neutral-700">
+              Great Job! <br /> You've completed the lesson.
+            </h1>
+            <div className="flex items-center gap-x-4 w-full">
+              <ResultCard
+                variant="points"
+                value={challenges.length * 10}
+              />
+              <ResultCard
+                variant="hearts"
+                value={hearts}
+              />
+            </div>
+          </div>
+          <Footer
+            lessonId={lessonId}
+            status="completed"
+            onCheck={() => router.push("/learn")}
+          />
+        </>
+      ) : (
+        <>
+          <Header
+            hearts={hearts}
+            percentage={percentage}
+            hasActiveSubscription={!!userSubscription?.isActive}
+          />
+          <div className="flex-1">
             <div className="h-full flex items-center justify-center">
               <div className="lg:min-h-[350px] lg:w-[600px] w-full px-6 lg:px-0 flex flex-col gap-y-12">
                 <h1 className="text-lg lg:text-3xl text-center lg:text-start font-bold text-neutral-700">
-                  {title}
+                  {challenge.type === "ASSIST" ? "Pilih jawaban yang benar" : challenge.question}
                 </h1>
                 <div>
                   {challenge.type === "ASSIST" && (
@@ -76,7 +203,7 @@ const [activeIndex, setActiveIndex] = useState(() => {
                     onSelect={onSelect}
                     status={status}
                     selectedOption={selectedOption}
-                    disabled={false}
+                    disabled={pending}
                     type={challenge.type}
                   />
                 </div>
@@ -84,10 +211,12 @@ const [activeIndex, setActiveIndex] = useState(() => {
             </div>
           </div>
           <Footer
-            disabled={!selectedOption}
+            disabled={pending || !selectedOption}
             status={status}
-            onCheck={() => {}}
+            onCheck={onContinue}
           />
         </>
-    );
+      )}
+    </>
+  );
 };
--- db/schema.ts
+++ db/schema.ts
@@ -1,5 +1,5 @@
 import { relations } from "drizzle-orm";
-import { boolean, integer, pgEnum, pgTable, serial, text } from "drizzle-orm/pg-core";
+import { boolean, integer, pgEnum, pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
 
 export const courses = pgTable("courses", {
   id: serial("id").primaryKey(),
@@ -30,7 +30,7 @@ export const unitsRelations = relations(units, ({ many, one }) => ({
 
 export const lessons = pgTable("lessons", {
   id: serial("id").primaryKey(),
-  title: text("text").notNull(),
+  title: text("title").notNull(),
   unitId: integer("unit_id").references(() => units.id, { onDelete: "cascade" }).notNull(),
   order: integer("order").notNull(),
 });
@@ -64,11 +64,10 @@ export const challengesRelations = relations(challenges, ({ one, many }) => ({
 
 export const challengeOptions = pgTable("challenge_options", {
   id: serial("id").primaryKey(),
-  challengeId: integer("challenge_id").references(() => challenges.id, { onDelete: "cascade" }).notNull(),
+  challengeId: integer("challenge").references(() => challenges.id, { onDelete: "cascade" }).notNull(),
   text: text("text").notNull(),
   correct: boolean("correct").notNull(),
   imageSrc: text("image_src"),
-  audioSrc: text("audio_src"),
 });
 
 export const challengeOptionsRelations = relations(challengeOptions, ({ one }) => ({
@@ -89,14 +88,14 @@ export const challengeProgressRelations = relations(challengeProgress, ({ one })
   challenge: one(challenges, {
     fields: [challengeProgress.challengeId],
     references: [challenges.id],
-  }), 
+  }),
 }));
 
 export const userProgress = pgTable("user_progress", {
   userId: text("user_id").primaryKey(),
   userName: text("user_name").notNull().default("User"),
-  userImageSrc: text("user_image_src").notNull().default("/logo.png"),
-  activeCourseId: integer("active_course_id").references(() => courses.id, { onDelete: "cascade" }),
+  userImageSrc: text("user_image_src").notNull().default("/green_hand_rock.png"),
+  activeCourseId: integer("active_user_id").references(() => courses.id, { onDelete: "cascade" }),
   hearts: integer("hearts").notNull().default(5),
   points: integer("points").notNull().default(0),
 });
@@ -107,3 +106,12 @@ export const userProgressRelations = relations(userProgress, ({ one }) => ({
     references: [courses.id],
   }),
 }));
+
+export const userSubscription = pgTable("user_subscription", {
+  id: serial("id").primaryKey(),
+  userId: text("user_id").notNull().unique(),
+  stripeCustomerId: text("stripe_customer_id").notNull().unique(),
+  stripeSubscriptionId: text("stripe_subscription_id").notNull().unique(),
+  stripePriceId: text("stripe_price_id").notNull(),
+  stripeCurrentPeriodEnd: timestamp("stripe_current_period_end").notNull(),
+});
